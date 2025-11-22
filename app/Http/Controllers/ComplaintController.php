c58aef26f0e37757900c41fb4685617b13dee606
iiiiu <iiiiu@test.com>
2025-11-22 15:42:03 +0500

    added complaints

--- app/Http/Controllers/ComplaintController.php
+++ app/Http/Controllers/ComplaintController.php
@@ -2,18 +2,69 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Complaint;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class ComplaintController extends Controller
 {
+    public function index(){
+        $complaints= Complaint::where('user_id', Auth::id())->get();
+        return response()->json([
+            'user'=>Auth::user(),
+            'complaints'=>$complaints
+        ]);
+    }
+
     public function store(Request $request){
-        $attributes=$request->validate([
-            'complaint'=>['required']
+    $attributes = $request->validate([
+        'complaint' => ['required', 'string', 'max:1000']
+    ]);
+
+    $complaint = Auth::user()->complaints()->create($attributes);
+
+    return response()->json([
+        'complaint' => $complaint
+    ]);
+}
+
+     /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'complaint' => 'required|string|max:1000',
         ]);
 
-        Auth::user()->complaints()->create($attributes);
+        $complaint = Complaint::where('user_id', Auth::id())->findOrFail($id);
+        $complaint->update([
+            'complaint' => $request->complaint,
+        ]);
 
-        return redirect('/index');
+        return response()->json([
+            'complaint'=>$complaint
+        ]);
+    }
+        public function destroy(string $id)
+    {
+        // Убедиться, что жалоба принадлежит текущему пользователю
+        $complaint = Complaint::where('user_id', Auth::id())->findOrFail($id);
+
+        $complaint->delete();
+
+        return response()->json([
+            'message' => 'Complaint deleted',
+            'id' => $id
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ComplaintController;
 use App\Http\Controllers\JsonSessionController;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
 
@@ -33,6 +34,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('auth:sanctum')->get('/complaints',[ComplaintController::class,'index']);
+Route::middleware('auth:sanctum')->post('/complaints',[ComplaintController::class,'store']);
+Route::middleware('auth:sanctum')->put('/complaints/{id}',[ComplaintController::class,'update']);
+Route::middleware('auth:sanctum')->delete('/complaints/{id}',[ComplaintController::class,'destroy']);
 
 
 
